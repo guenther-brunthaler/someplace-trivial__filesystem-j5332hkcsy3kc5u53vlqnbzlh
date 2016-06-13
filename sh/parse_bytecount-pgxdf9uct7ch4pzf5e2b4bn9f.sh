b782caaff6889d4062a5082108673e03616a0c10
Guenther Brunthaler <gb@xrasp.emgenxx69lwyn5ctlr4nl64ul.local>
2016-06-13 23:25:57 +0200

    Insert allocations with absolute range position
    Relative ranges are not processed yet. Also the changes will not yet be
    written back to the TrivialFS.

--- sh/parse_bytecount-pgxdf9uct7ch4pzf5e2b4bn9f.sh
+++ sh/parse_bytecount-pgxdf9uct7ch4pzf5e2b4bn9f.sh
@@ -1,22 +1,38 @@
 # "42", "42 B", "42 kB", "42 kiB", "42MB, "42GiB", "42TB", "42 PiB", ...
 parse_bytecount() {
 	local num tx unit mult
-	num=`expr x"$1" : '\(x0\|x[1-9][0-9]*\)'`; num=${num#x}
+	num=`
+		LC_COLLATE=C expr x"$1" : '\(x0\)' \
+			\| x"$1" : '\(x[1-9][0-9]*\)' \
+			\| x
+	`
+	num=${num#x}; test -n "$num" || return
 	tx=${1#"$num"}
 	if test -n "$tx"
 	then
-		unit=`expr x"$tx" : x'[[:space:]]*\(\([kMGTPEZY]i\?\)\?B\)$'`
+		unit=`
+			rx='[[:space:]]*\(\([kKMGTPEZY]i\{0,1\}\)\{0,1\}B\)$'
+			LC_COLLATE=C expr x"$tx" : x"$rx"
+		`
+		unit=`
+			rx1='\(\([MGTPEZY]i\{0,1\}\)\{0,1\}B\)$'
+			rx2='\(KiB\)$'
+			rx3='\(kB\)$'
+			LC_COLLATE=C expr x"$unit" : x"$rx1" \
+				\| x"$unit" : x"$rx2" \
+				\| x"$unit" : x"$rx3"
+		`
 		unit=${unit%?}
 		case $unit in
 			"") unit=B;;
 			*i) unit=${unit%?}; mult=1024;;
 			*) mult=1000
 		esac
-		unit=`echo $unit | tr BkMGTPEZY 012345678`
+		unit=`echo $unit | LC_COLLATE=C tr BkKMGTPEZY 0112345678`
 		while test $unit != 0
 		do
-			tx=`expr $num '*' $mult`
-			test $tx -gt $num
+			tx=`expr $num '*' $mult || :`
+			test $tx -ge $num
 			num=$tx
 			unit=`expr $unit - 1 || :`
 		done
